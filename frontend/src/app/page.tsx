2171dffa19efd0e2dd3b21081339389923c4e853
Kazuglobal <153802620+Kazuglobal@users.noreply.github.com>
2025-03-16 14:15:44 +0900

    Update main page to include TeamInfo component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,43 +1,47 @@
-'use client';
-
 import { useState } from 'react';
 import {
+  Box,
   Container,
-  VStack,
   Tabs,
   TabList,
   TabPanels,
   Tab,
   TabPanel,
 } from '@chakra-ui/react';
-import { TeamSelector } from '../components/TeamSelector';
-import { PlayerList } from '../components/PlayerList';
+import TeamSelector from '../components/TeamSelector';
+import PlayerList from '../components/PlayerList';
 import StatsViewer from '../components/StatsViewer';
+import TeamInfo from '../components/TeamInfo';
 
 export default function Home() {
   const [selectedTeam, setSelectedTeam] = useState<string | null>(null);
 
   return (
     <Container maxW="container.xl" py={8}>
-      <VStack spacing={8} align="stretch">
-        <Tabs>
-          <TabList>
-            <Tab>チーム/選手</Tab>
-            <Tab>成績</Tab>
-          </TabList>
-          <TabPanels>
-            <TabPanel>
-              <VStack spacing={8} align="stretch">
-                <TeamSelector onTeamSelect={setSelectedTeam} />
-                <PlayerList teamId={selectedTeam} />
-              </VStack>
-            </TabPanel>
-            <TabPanel>
-              <StatsViewer />
-            </TabPanel>
-          </TabPanels>
-        </Tabs>
-      </VStack>
+      <Tabs>
+        <TabList>
+          <Tab>チーム/選手</Tab>
+          <Tab>チーム情報</Tab>
+          <Tab>統計情報</Tab>
+        </TabList>
+
+        <TabPanels>
+          <TabPanel>
+            <Box mb={6}>
+              <TeamSelector onTeamSelect={setSelectedTeam} />
+            </Box>
+            {selectedTeam && <PlayerList teamId={selectedTeam} />}
+          </TabPanel>
+
+          <TabPanel>
+            <TeamInfo />
+          </TabPanel>
+
+          <TabPanel>
+            <StatsViewer />
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
     </Container>
   );
 }
